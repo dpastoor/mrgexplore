c2ddbb283dc8f49eaa791a82b457d0ca15fc6688
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-31 18:24:20 -0400

    mrgexplore function naive implementation

--- simple_test/app.R
+++ simple_test/app.R
@@ -3,12 +3,18 @@ source("server.R")
 library(shiny)
 mod <- mread("one_cmpt_f")
 mod <- mod %>% ev(amt=100, ii=6,addl=8)     
-    ##' Extract annotations
-params <- mrgsolve:::details(mod) %>% 
-  filter(block == "PARAM")
-mod_params <- as.list(param(mod))
-shiny_param_list <- mod_params[which(names(mod_params) %in% params$name)]
-names(shiny_param_list)
-ui <- make_ui(shiny_param_list)
-server <- make_server(mod, shiny_param_list)
-shinyApp(ui = ui, server = server)
+##' Extract annotations
+
+mrgexplore <- function(mod) {
+  # only params should be editable in sliders (for now at least)
+  params <- mrgsolve:::details(mod) %>% 
+    filter(block == "PARAM")
+  mod_params <- as.list(param(mod))
+  shiny_param_list <- mod_params[which(names(mod_params) %in% params$name)]
+  names(shiny_param_list)
+  ui <- make_ui(shiny_param_list)
+  server <- make_server(mod, shiny_param_list)
+  shinyApp(ui = ui, server = server)
+}
+
+mod %>% mrgexplore
