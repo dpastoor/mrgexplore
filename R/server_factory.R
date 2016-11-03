339de5319005bd4f6d94e7a3a42f4ba995552481
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-02 22:35:15 -0400

    vector instead of list for server as well

--- R/server_factory.R
+++ R/server_factory.R
@@ -6,9 +6,10 @@
 #' prior filtering. Any passed in will expect to be modifying
 #' the values from the slider inputs
 #' @export
-make_server <- function(mod, shiny_param_list) {
+make_server <- function(.model, shiny_param_list) {
   return(function(input, output, session) {
-    idata <- as.list(mrgsolve::param(mod)) %>% dplyr::bind_cols() 
+    idata <- as.numeric(param(.model)) %>% 
+      as.list %>% dplyr::bind_cols() 
     param_names <- names(shiny_param_list)
     nidata <- reactive({
       dots <- lapply(param_names, function(param) {
@@ -18,7 +19,7 @@ make_server <- function(mod, shiny_param_list) {
     })
     output$default_plot <- renderPlot({
       print(nidata())
-      mod %>% 
+      .model %>% 
         mrgsolve::mrgsim(idata = nidata()) %>% 
         plot
     })
