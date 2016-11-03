d867d8c9d78222a077c486939d2623d3faab8204
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-02 22:05:19 -0400

    mrgexplore docs

---
+++ R/mrgexplore.R
@@ -0,0 +1,11 @@
+#' launch shiny app to explore influence of parameter values
+#' @param mod mrgsolve model
+#' @export
+mrgexplore <- function(mod) {
+  # only params should be editable in sliders (for now at least)
+  shiny_param_list <- as.list(mrgsolve::param(mod))
+  ui <- make_ui(shiny_param_list)
+  server <- make_server(mod, shiny_param_list)
+  shinyApp(ui = ui, server = server)
+}
+
